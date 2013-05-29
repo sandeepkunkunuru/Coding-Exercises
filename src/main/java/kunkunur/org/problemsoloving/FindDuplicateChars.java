aacff9caf03acf058d1f38823e386c67b6d4f805
Sandeep Kunkunuru <sandeep.kunkunuru@visualiq.com>
2013-05-29 13:55:01 -0400

    Made a few package changes and updated README.

---
+++ src/main/java/kunkunur/org/problemsoloving/FindDuplicateChars.java
@@ -0,0 +1,76 @@
+/**
+ * Implement an algorithm to determine if a string has all unique characters.
+ * What if you can not use additional data structures?
+ */
+package kunkunur.org.problemsoloving;
+
+/**
+ * @author kunkunur
+ *
+ */
+public class FindDuplicateChars {
+	private static final String input1 = "Sandeep1";
+	private static final String input2 = "uniqe";
+	private static final String input3 = " not so unique";
+
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		//With Additinal data structures
+		System.out.println("Input '" + input1 + "' has unique letters :" + !containsDuplicateChars(input1));
+		System.out.println("Input '" + input2 + "' has unique letters :" + !containsDuplicateChars(input2));
+		System.out.println("Input '" + input3 + "' has unique letters :" + !containsDuplicateChars(input3));
+		
+		//Without Additional data structures
+		System.out.println("Input '" + input1 + "' has unique letters :" + !containsDuplicateChars2(input1));
+		System.out.println("Input '" + input2 + "' has unique letters :" + !containsDuplicateChars2(input2));
+		System.out.println("Input '" + input3 + "' has unique letters :" + !containsDuplicateChars2(input3));
+		
+	}
+
+
+	/**
+	 * Creates additional data structures
+	 * 
+	 * @param input
+	 * @return
+	 */
+	private static boolean containsDuplicateChars(String input) {
+		boolean[] letterCount = new boolean[256];
+		
+		for (char letter : input.toCharArray()) {
+			if(letterCount[letter % 256] == false) {
+				letterCount[letter % 256] = true;
+			} else { 
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	
+
+	/**
+	 * Does not create additional data structures 
+	 * @param input
+	 * @return
+	 */
+	private static boolean containsDuplicateChars2(String input) {
+		int size = input.length();
+		
+		for (int i = 0; i < size; i++) {
+			char letter1 = input.charAt(i);
+			
+			for (int j = i + 1; j < size; j++) {
+				if (letter1 == input.charAt(j)){
+					return true;
+				}
+			}
+		}
+		
+		return false;
+	}
+}
---
+++ src/main/java/kunkunur/org/problemsoloving/datastructures/Queue.java
@@ -0,0 +1,59 @@
+package kunkunur.org.problemsoloving.datastructures;
+
+public class Queue<T> {
+    private QueueNode<T> first;
+    private QueueNode<T> last;
+
+    public Queue(T item) {
+        first = new QueueNode<T>(item);
+        last = first;
+    }
+
+    public void enqueue(T item) {
+        QueueNode<T> newNode =  new QueueNode<T>(item);
+
+        if (first == null) {
+            last = first = newNode;
+        } else {
+            last.setNext(newNode);
+            last = newNode;
+        }
+    }
+
+    public T dequeue() {
+        QueueNode<T> currentFirst = first;
+
+        if (first == null){
+        	return null;
+        } else if (last == first) {
+            last = first = null;
+        } else {
+            first = currentFirst.getNext();
+        }
+
+        return currentFirst.getValue();
+    }
+
+    private class QueueNode<T> {
+        private T value;
+        private QueueNode<T> next;
+
+        QueueNode(T val) {
+            value = val;
+
+        }
+
+        QueueNode<T> getNext() {
+            return next;
+        }
+
+        void setNext(QueueNode<T> next) {
+            this.next = next;
+        }
+
+        T getValue() {
+            return value;
+        }
+    }
+
+}
