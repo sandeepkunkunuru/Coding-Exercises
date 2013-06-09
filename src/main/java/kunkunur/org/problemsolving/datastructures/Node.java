51b53f52765cf4b84aa612947efe293a0fa48cf7
Sandeep Kunkunuru <sandeep.kunkunuru@visualiq.com>
2013-06-09 16:52:06 -0400

    1. A simple implementation of linkedlist 2. Find nth to last node in a linked list.

---
+++ src/main/java/kunkunur/org/problemsolving/datastructures/Node.java
@@ -0,0 +1,40 @@
+package kunkunur.org.problemsolving.datastructures;
+
+/**
+ * A very simple implementation of linkedlist where node itself is overloaded with
+ * the responsibilities of the list
+ * @author kunkunur
+ *
+ */
+public class Node<T> {
+	Node<T> next;
+	T value;
+	
+	public Node(T value){
+		this.value = value; 
+	}
+	
+	public Node<T> next(){
+		return next;
+	}
+	
+	public T value(){
+		return value;
+	}
+	/**
+	 * 
+	 * @param value
+	 * @return newly added node
+	 */
+	public Node<T> addNodeToTail(T value){
+		Node<T> temp = this;
+
+		while(temp.next != null){
+			temp = temp.next;
+		}
+		
+		temp.next = new Node<T>(value);
+		
+		return temp.next;
+	} 
+}
