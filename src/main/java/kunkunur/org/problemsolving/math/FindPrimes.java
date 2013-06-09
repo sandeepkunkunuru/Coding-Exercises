d436ae2fe5faa92136b977de27e756ddbc5a28ad
Sandeep Kunkunuru <sandeep.kunkunuru@visualiq.com>
2013-06-09 13:17:43 -0400

    Find primes: Iterative solution

---
+++ src/main/java/kunkunur/org/problemsolving/math/FindPrimes.java
@@ -0,0 +1,44 @@
+package kunkunur.org.problemsolving.math;
+
+import java.util.Arrays;
+
+public class FindPrimes {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int boundary = 23;
+
+		findPrimes(boundary);
+	}
+
+	private static void findPrimes(int boundary) {
+		//int primes[] = new int[(int) Math.floor(Math.sqrt(boundary))];
+		
+		int primes[] = new int[boundary / 2];
+		primes[0] = 2;
+
+		int currentSize = 1;
+
+		for (int i = 3; i < boundary; i++) {
+			boolean newprime = true;
+			
+			for (int j = 0; j < currentSize; j++) {
+//				System.out.println(primes[j]);
+				if ((i % primes[j]) == 0) {
+					newprime = false;
+					break;
+				}
+			}
+			
+			if (newprime) {
+				primes[currentSize] = i;
+				currentSize++;
+			}			
+		}
+		
+		System.out.println(Arrays.toString(primes));
+	}
+
+}
