40b873b84759fe46bf9358564907d6010994f8a8
Sandeep Kunkunuru <sandeep.kunkunuru@visualiq.com>
2013-06-10 23:13:23 -0400

    Sort Stack using stack

---
+++ src/main/java/kunkunur/org/problemsolving/SortStack.java
@@ -0,0 +1,44 @@
+package kunkunur.org.problemsolving;
+
+import kunkunur.org.problemsolving.datastructures.StackNode;
+
+public class SortStack {
+
+	public static void main(String[] args) {
+		StackNode<Integer> stack1 = new StackNode<Integer>(34).push(45).push(56).push(12).push(3);
+		
+		//Final End state = 3 --> 12 --> 34--> 45 -->56
+		
+		//Lets build another stack which can be used to rearrange.
+		StackNode<Integer> stack2 = new StackNode<Integer>(stack1.pop());
+		
+		while(true){
+			int stack1Top = stack1.pop();
+			
+			int stack2Peek = stack2.peek();
+			
+			if(stack1Top > stack2Peek){
+				stack2.push(stack1Top);
+			} else {
+				while(stack2.peek() > stack1Top){
+					stack1.push(stack2.pop());
+				}
+				
+				stack2.push(stack1Top);
+			}
+			
+			if(stack1.isEmpty()){
+				while(!stack2.isEmpty()){
+					stack1.push(stack2.pop());
+				}
+				break;
+			}
+		}
+		
+		while(!stack1.isEmpty()){
+			System.out.println(stack1.pop());
+		}
+		
+	}
+
+}
---
+++ src/main/java/kunkunur/org/problemsolving/datastructures/StackNode.java
@@ -0,0 +1,54 @@
+package kunkunur.org.problemsolving.datastructures;
+
+public class StackNode<T> {
+	StackNode<T> previous;
+	StackNode<T> top;
+	T value;
+	
+	public StackNode(T value){
+		this.previous = null;
+		this.value = value;
+		top = this;
+	}
+	
+	public StackNode<T> push(T value){
+		StackNode<T> newNode = new StackNode<T>(value);
+		
+		if(top == null) {
+			top = newNode;
+		} else {
+			StackNode<T> prevTop = top;
+			top = newNode;
+			top.previous = prevTop;
+		}	
+		
+		return this;
+	}
+	
+	public T pop(){
+		StackNode<T> topNode = null;
+		
+		if(top != null) {
+			topNode = top;
+	
+			top = topNode.previous;
+			
+			topNode.previous = null;			
+		}
+		
+		return topNode.value;
+	}
+	
+	public T peek(){
+		if(top != null){
+			return top.value;
+		}
+		
+		return null;
+	}
+	
+	public boolean isEmpty(){
+		return top == null;
+	}
+
+}
