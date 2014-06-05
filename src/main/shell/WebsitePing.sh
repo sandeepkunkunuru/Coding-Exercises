090d256d1510e2c629eca62e4c43df5be2b9141e
sandeep kunkunuru <sandeep.kunkunuru@visualiq.com>
2014-06-05 14:11:00 -0400

    Shell script to check if website is up

---
+++ src/main/shell/WebsitePing.sh
@@ -0,0 +1,11 @@
+rm -f response.html
+wget $1 -o response.html
+grep -qs "401 Unauthorized" response.html
+OUT=$?
+if [ $OUT -eq 0 ];then
+   echo "$1 is up"
+   exit 0
+else
+   echo "$1 is down"
+   exit 1
+fi
